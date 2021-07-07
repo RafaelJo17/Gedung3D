0d2c240eaf0a4e3c302ec34f822e51abfd43b164
dece441 <73749023+dece441@users.noreply.github.com>
2021-07-07 14:51:36 +0700

    Update Robinson.cpp

--- Gedung3D/Robinson.cpp
+++ Gedung3D/Robinson.cpp
@@ -93,7 +93,7 @@ void kubus(float red, float green, float blue, float x, float y, float z, float 
 }
 
 void meja_kotak(float x, float y, float z) {
-    kubus(0.396f, 0.337f, 0.243f, x, y+4, z, 10,10,1, 0);
+    silinder(0.813f, 0.694f, 0.494f, x, -z, y+(4*2), 5, 0.2);
     kubus(0.813f, 0.694f, 0.494f, x, y, z, 1, 1, 4, 0);
     silinder(0.813f, 0.694f, 0.494f, x, -z, y, 3, 0.2);
 }
@@ -256,8 +256,8 @@ void display(void)
     //gedung mall belakang
     kubus(0.3, 0.3, 0.3, -34, 0, -150, 518, 290, 100.1, 0);
 
-    meja_kotak(0,0,200);
-    meja_kotak(30, 0, 200);
+    meja_kotak(0, 0, 80);
+    meja_kotak(30, 0, 100);
 
     glPopMatrix();
 
